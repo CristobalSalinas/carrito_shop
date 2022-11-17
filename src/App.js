ae02d529856815761ccbbab3a0579634cea45307
CristobalSalinas <contacto.csalinas@gmail.com>
2022-11-17 15:22:33 +0100

    :art: agregando imagenes y lista de productos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import { Component } from "react";
 
 class App extends Component{
+
+  state = {
+    productos:[
+      {name:'Tomate',price:1500,img:'/productos/tomate.jpg'},
+      {name:'Arbejas',price:2500,img:'/productos/arbejas.jpg'},
+      {name:'Lechuga',price:500,img:'/productos/lechuga.jpg'},
+    ]
+  };
+
   render(){
     return(
       <div>
