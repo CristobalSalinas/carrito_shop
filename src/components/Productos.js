00f52e8495b05ba27d7bcb198c822a38ced32e6b
CristobalSalinas <contacto.csalinas@gmail.com>
2022-11-17 16:00:25 +0100

    :art: agregando estilos a los productos

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,13 +1,21 @@
 import { Component } from "react"; 
 import Producto from './Producto';
 
+const styles= {
+    productos:{
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-between'
+    }
+};
+
 class Productos extends Component{
     render(){
         // Extraigo las props
         const {productos,agregarAlCarro} = this.props;
 
         return(
-            <div>
+            <div style={styles.productos}>
                 {productos.map(producto=><Producto 
                     agregarAlCarro={agregarAlCarro} 
                     key={producto.name} 
